79ac31ddf9f5fe2201a826bdc708b49c70726bd7
YangWoongmo <yom3795@gmail.com>
2023-06-23 18:34:49 +0900

    BOJ1008

---
+++ Algorithm/BOJ1008/BOJ1008.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+
+int main()
+{
+    double a, b;
+    double result;
+
+    std::cin >> a >> b;
+
+    result = a / b;
+    std::cout.precision(9);
+    std::cout << std::fixed;
+    std::cout << result;
+
+    return 0;
+}
