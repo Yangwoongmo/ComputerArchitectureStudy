cf6ba6c49e3ddaf2fa6329b045f28a247d7d4f4d
YangWoongmo <yom3795@gmail.com>
2023-06-29 07:32:41 +0900

    BOJ2741

---
+++ Algorithm/BOJ2741/BOJ2741.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int num;
+    cin >> num;
+
+    for (int i = 1; i <= num; i++)
+    {
+        cout << i << '\n'; // << endl 사용시 timeOut
+    }
+
+    return 0;
+}
