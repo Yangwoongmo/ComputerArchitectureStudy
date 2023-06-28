1a557ff545cefc9cd61d60e659f9b6d475aa8b66
YangWoongmo <yom3795@gmail.com>
2023-06-29 07:32:14 +0900

    BOJ2562

---
+++ Algorithm/BOJ2562/BOJ2562.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int count[9] = { 0, };
+    int max = 0;
+    int index = 0;
+
+    for (int i = 0; i < 9; i++)
+    {
+        cin >> count[i];
+    }
+
+    for (int i = 0; i < 9; i++)
+    {
+        if (count[i] > max)
+        {
+            index = i + 1;
+            max = count[i];
+        }
+    }
+
+    cout << max << endl << index;
+
+    return 0;
+}
