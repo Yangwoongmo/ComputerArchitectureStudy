74c33d6807976f82cff6f5a3196671cc80e8639c
YangWoongmo <yom3795@gmail.com>
2023-06-29 08:19:33 +0900

    Create BOJ2753.cpp

---
+++ Algorithm/BOJ2753/BOJ2753.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int year;
+    int CaseA, CaseB, CaseC = 0;
+    cin >> year;
+
+    CaseA = year % 4;
+    CaseB = year % 100;
+    CaseC = year % 400;
+
+    if ((CaseA == 0 && CaseB != 0) || CaseC == 0)
+    {
+        cout << '1';
+    }
+    else
+    {
+        cout << '0';
+    }
+
+    return 0;
+}
