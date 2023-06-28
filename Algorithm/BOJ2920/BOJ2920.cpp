5304e89447144fd90ad953c8943ed4431d184864
YangWoongmo <yom3795@gmail.com>
2023-06-29 08:19:38 +0900

    Create BOJ2920.cpp

---
+++ Algorithm/BOJ2920/BOJ2920.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    char scale[8];
+    string str = {};
+
+    for (int i = 0; i < 8; i++)
+    {
+        cin >> scale[i];
+        str.push_back(scale[i]);
+    }
+
+    if (str == "12345678")
+    {
+        cout << "ascending";
+    }
+    else if (str == "87654321")
+    {
+        cout << "descending";
+    }
+    else
+    {
+        cout << "mixed";
+    }
+
+    return 0;
+}
