8cde09d89b1942eec9849e9467cdb6651c8c9bb4
YangWoongmo <yom3795@gmail.com>
2023-06-29 08:19:35 +0900

    Create BOJ2884.cpp

---
+++ Algorithm/BOJ2884/BOJ2884.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int hour, minute;
+    int lossMinute;
+    cin >> hour >> minute;
+
+    lossMinute = minute - 45;
+
+    if (lossMinute < 0)
+    {
+        hour--;
+        minute = 60 + lossMinute;
+    }
+    else
+    {
+        minute -= 45;
+    }
+
+    if (hour < 0)
+    {
+        hour = 24 + hour;
+    }
+
+    cout << hour << ' ' << minute;
+
+    return 0;
+}
