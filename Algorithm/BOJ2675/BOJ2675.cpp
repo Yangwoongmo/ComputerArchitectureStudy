865252d29c4a0a80f6eba24efc5e0e89acde17e9
YangWoongmo <yom3795@gmail.com>
2023-06-29 07:32:27 +0900

    BOJ2675

---
+++ Algorithm/BOJ2675/BOJ2675.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    int testCase;
+
+    cin >> testCase;
+
+    for (int i = 0; i < testCase; i++)
+    {
+        int loop;
+        string str;
+
+        cin >> loop >> str;
+
+        for (int j = 0; j < str.length(); j++)
+        {
+            for (int k = 0; k < loop; k++)
+            {
+                cout << str[j];
+            }
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
