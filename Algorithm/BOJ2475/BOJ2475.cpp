b8a21d49f39cdcc4e1390f1cd56075d1816136db
YangWoongmo <yom3795@gmail.com>
2023-06-29 07:32:07 +0900

    BOJ2475

---
+++ Algorithm/BOJ2475/BOJ2475.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+
+int Square(int i)
+{
+    i = i * i;
+    return i;
+}
+
+using namespace std;
+
+int main()
+{
+    int a, b, c, d, e = 0;
+    int result = 0;
+
+    cin >> a >> b >> c >> d >> e;
+
+    result = (Square(a) + Square(b) + Square(c) + Square(d) + Square(e)) % 10;
+
+    cout << result;
+
+    return 0;
+}
