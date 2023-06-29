bbae51e4afdb7a71c3fb055c6696a994143b53bd
YangWoongmo <yom3795@gmail.com>
2023-06-29 23:27:02 +0900

    Create BOJ3052.cpp

---
+++ Algorithm/BOJ3052/BOJ3052.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int array[10] = { 0, };
+    int remainArray[42] = {0,};
+    int result = 0;
+
+    for (int i = 0; i < 10; i++)
+    {
+        cin >> array[i];
+    }
+
+    for (int i = 0; i < 10; i++)
+    {
+        int index = array[i] % 42;
+        remainArray[index]++;
+    }
+
+    for (int i = 0; i < 42; i++)
+    {
+        if (remainArray[i] > 0)
+        {
+            result++;
+        }
+    }
+
+    cout << result;
+
+    return 0;
+}
