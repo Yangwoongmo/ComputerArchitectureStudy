8add822aa4a1cbbac6fe8f1180308991c65fc0a9
YangWoongmo <yom3795@gmail.com>
2023-06-29 23:27:00 +0900

    Create BOJ10250.cpp

---
+++ Algorithm/BOJ10250/BOJ10250.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int testCase;
+    cin >> testCase;
+
+    for (int i = 0; i < testCase; i++)
+    {
+        int height, weight, num;
+        int result;
+        cin >> height >> weight >> num;
+
+        if (num % height == 0)
+        {
+            result = height * 100 + (num / height);
+        }
+        else
+        {
+            result = (num % height) * 100 + (num / height) + 1;
+        }
+
+        cout << result << '\n';
+    }
+
+    return 0;
+}
