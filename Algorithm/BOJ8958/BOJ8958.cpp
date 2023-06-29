3cb1b7974a4d9ac4efc5473db49d258037d78f81
YangWoongmo <yom3795@gmail.com>
2023-06-29 23:27:04 +0900

    Create BOJ8958.cpp

---
+++ Algorithm/BOJ8958/BOJ8958.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    int testCase;
+
+    cin >> testCase;
+
+    for (int i = 0; i < testCase; i++)
+    {
+        string ox;
+        int result = 0;
+        int point = 0;
+
+        cin >> ox;
+
+        for (int j = 0; j < ox.length(); j++)
+        {
+            if (ox[j] == 'O')
+            {
+                point++;
+                result += point;
+            }
+            else
+            {
+                point = 0;
+            }
+        }
+        cout << result << '\n';
+    }
+
+    return 0;
+}
