4e0e22ba2fc59319aea15d63bd0158b7cee39e41
YangWoongmo <yom3795@gmail.com>
2023-07-01 07:03:29 +0900

    Create BOJ10818.cpp

---
+++ Algorithm/BOJ10818/BOJ10818.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <limits.h>
+
+using namespace std;
+
+int main()
+{
+    int max = INT_MIN;
+    int min = INT_MAX;
+    int count;
+    cin >> count;
+
+    for (int i = 0; i < count; i++)
+    {
+        int a;
+        cin >> a;
+
+        if (max < a)
+        {
+            max = a;
+        }
+        if (min > a)
+        {
+            min = a;
+        }
+    }
+    
+    cout << min << ' ' << max;
+
+    return 0;
+}
