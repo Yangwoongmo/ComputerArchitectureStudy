b90ecfd8e21d06fb96b3f53d5cdd22b4f717cbd5
YangWoongmo <yom3795@gmail.com>
2023-07-01 07:03:19 +0900

    Create BOJ10809.cpp

---
+++ Algorithm/BOJ10809/BOJ10809.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string s;
+    int resultArray[26] = { 0, };
+    cin >> s;
+
+    for (int i = 0; i < 26; i++)
+    {
+        resultArray[i] = -1;
+    }
+
+    for (int i = 0; i < s.length(); i++)
+    {
+        int index = s[i] - 97; // ASC a == 97
+
+        if (resultArray[index] == -1)
+        {
+            resultArray[index] = i;
+        }
+    }
+
+    for (int i = 0; i < 26; i++)
+    {
+        cout << resultArray[i] << ' ';
+    }
+
+    return 0;
+}
