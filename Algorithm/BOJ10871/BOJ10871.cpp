72827b27a164d87b433ce13df6e5c276dbf2c7d7
YangWoongmo <yom3795@gmail.com>
2023-07-01 07:03:33 +0900

    Create BOJ10871.cpp

---
+++ Algorithm/BOJ10871/BOJ10871.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int n, x;
+    cin >> n >> x;
+
+    for (int i = 0; i < n; i++)
+    {
+        int a;
+        cin >> a;
+        if (a < x)
+        {
+            cout << a << ' ';
+        }
+    }
+
+    return 0;
+}
