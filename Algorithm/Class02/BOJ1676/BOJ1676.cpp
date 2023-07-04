c96f1af46b31f5ca2b8d0dcbd702b42ee439544f
YangWoongmo <yom3795@gmail.com>
2023-07-05 06:24:54 +0900

    BOJ1676

---
+++ Algorithm/Class02/BOJ1676/BOJ1676.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int Factorial(int num)
+{
+    int count = 0;
+    for (int i = 1; i <= num; i++)
+    {
+        int temp = i;
+
+        while (true)
+        {
+            if (temp % 5 == 0)
+            {
+                count++;
+                temp /= 5;
+            }
+            else
+            {
+                break;  
+            }
+        }
+    }
+    return count;
+}
+
+int main()
+{
+    int num;
+    cin >> num;
+    cout << Factorial(num);
+
+    return 0;
+}
