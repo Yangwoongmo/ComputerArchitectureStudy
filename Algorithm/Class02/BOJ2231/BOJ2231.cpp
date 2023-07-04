a3105c5365365643e483e17399ed06b40ce47fd4
YangWoongmo <yom3795@gmail.com>
2023-07-05 07:30:55 +0900

    Create BOJ2231.cpp

---
+++ Algorithm/Class02/BOJ2231/BOJ2231.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int num;
+    int result = 0;
+    cin >> num;
+
+    for (int i = 1; i < num; i++)
+    {
+        int sum = 0;
+        int count = i;
+
+        while (count != 0)
+        {
+            sum += count % 10;
+            count /= 10;
+        }
+
+        if (sum + i == num)
+        {
+            result = i;
+            break;
+        }
+    }
+
+    cout << result;
+
+    return 0;
+}
