622d7eed45014c5830e6be58ceb7b6bf952b3ab8
YangWoongmo <yom3795@gmail.com>
2023-07-05 07:18:52 +0900

    Create BOJ1978.cpp

---
+++ Algorithm/Class02/BOJ1978/BOJ1978.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int loop;
+    int result = 0;
+    cin >> loop;
+
+    for (int i = 0; i < loop; i++)
+    {
+        int temp;
+        int count = 0;
+        cin >> temp;
+
+        for (int j = 1; j <= temp; j++)
+        {
+            if (temp % j == 0)
+            {
+                count++;
+            }
+        }
+
+        if (count == 2)
+        {
+            result++;
+        }
+    }
+
+    cout << result;
+
+    return 0;
+}
