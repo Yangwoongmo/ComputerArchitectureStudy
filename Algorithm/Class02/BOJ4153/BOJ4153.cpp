089cc241c594a4fb632934f8ba35bc73df39450b
YangWoongmo <yom3795@gmail.com>
2023-07-05 07:18:50 +0900

    Create BOJ4153.cpp

---
+++ Algorithm/Class02/BOJ4153/BOJ4153.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+
+using namespace std;
+
+bool isRightTriangle(int a, int b, int c)
+{
+    a *= a;
+    b *= b;
+    c *= c;
+    if ((a + b == c) || (b + c == a) || (c + a == b))
+    {
+        return true;
+    }
+    return false;
+}
+
+int main()
+{
+    while (true)
+    {
+        int a, b, c;
+        cin >> a >> b >> c;
+
+        if (a == 0)
+        {
+            break;
+        }
+
+        bool result = isRightTriangle(a, b, c);
+        if (result)
+        {
+            cout << "right\n";
+        }
+        else
+        {
+            cout << "wrong\n";
+        }
+    }
+
+    return 0;
+}
