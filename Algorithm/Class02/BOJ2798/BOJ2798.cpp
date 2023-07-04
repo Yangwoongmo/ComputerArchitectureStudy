a3d08553aeefb888037ae37d2dbc50f91631577d
YangWoongmo <yom3795@gmail.com>
2023-07-05 07:59:22 +0900

    Create BOJ2798.cpp

---
+++ Algorithm/Class02/BOJ2798/BOJ2798.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int cardArray[101];
+
+int main()
+{
+    int N, M;
+    int maxCount = 0;
+    int temp = 0;
+    cin >> N >> M;
+
+    for (int i = 0; i < N; i++)
+    {
+        cin >> cardArray[i];
+    }
+
+    for (int i = 0; i < N - 2; i++)
+    {
+        for (int j = i + 1; j < N - 1; j++)
+        {
+            for (int k = j + 1; k < N; k++)
+            {
+                temp = cardArray[i] + cardArray[j] + cardArray[k];
+
+                if (temp <= M)
+                {
+                    maxCount = max(temp, maxCount);
+                }
+            }
+        }
+    }
+
+    cout << maxCount;
+
+    return 0;
+}
