4fc48554597a5a0828245748bfb0175515f5ee18
YangWoongmo <yom3795@gmail.com>
2023-07-05 07:02:30 +0900

    Create BOJ1874.cpp

---
+++ Algorithm/Class02/BOJ1874/BOJ1874.cpp
@@ -0,0 +1,49 @@
+﻿#include <iostream>
+#include <stack>
+
+using namespace std;
+
+stack<int> intStack;
+string str;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int loop;
+    int count = 1;
+    cin >> loop;
+
+    for(int i = 0; i < loop; i++)
+    {
+        int num;
+        cin >> num;
+
+        while (count <= num)
+        {
+            intStack.push(count);
+            count++;
+            str.push_back('+');
+        }
+
+        if (intStack.top() == num)
+        {
+            intStack.pop();
+            str.push_back('-');
+        }
+        else
+        {
+            cout << "NO";
+            return 0;
+        }
+    }
+
+    for (int i = 0; i < str.length(); i++)
+    {
+        cout << str[i] << '\n';
+    }
+
+    return 0;
+}
