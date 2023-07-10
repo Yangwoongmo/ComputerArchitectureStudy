296c1e481136e3e74f9c468560e7342507e6d853
YangWoongmo <yom3795@gmail.com>
2023-07-10 15:12:19 +0900

    프로그래머스 추억 점수

---
+++ Algorithm/프로그래머스/추억 점수/Answer.cpp
@@ -0,0 +1,29 @@
+﻿#include <string>
+#include <vector>
+#include <map>
+
+using namespace std;
+// https://school.programmers.co.kr/learn/courses/30/lessons/176963
+vector<int> solution(vector<string> name, vector<int> yearning, vector<vector<string>> photo) {
+    
+    map<string, int> m1;
+    vector<int> answer;
+
+    for (int i = 0; i < name.size(); i++)
+    {
+        m1[name[i]] = yearning[i];
+    }
+
+    for (int i = 0; i < photo.size(); i++)
+    {
+        int allScore = 0;
+        for (int j = 0; j < photo[i].size(); j++)
+        {
+            int score = m1[photo[i][j]];
+            allScore += score;
+        }
+        answer.push_back(allScore);
+    }
+
+    return answer;
+}
