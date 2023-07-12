6f8328e8400001f6347d36159fb59c505e724cf1
YangWoongmo <yom3795@gmail.com>
2023-07-12 14:57:22 +0900

    BOJ10810

---
+++ Algorithm/Class01/BOJ10810/BOJ10810Array.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int basket[100];
+
+int main()
+{
+    int N, M;
+    int start, end, ballNum;
+
+    cin >> N >> M;
+
+    for (int i = 0; i < M; i++)
+    {
+        cin >> start >> end >> ballNum;
+
+        for (int startIndex = start - 1; startIndex <= end - 1; startIndex++)
+        {
+            basket[startIndex] = ballNum;
+        }
+    }
+
+    for (int i = 0; i < N; i++)
+    {
+        cout << basket[i] << ' ';
+    }
+
+    return 0;
+}
---
+++ Algorithm/Class01/BOJ10810/BOJ10810Vector.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    int N, M;
+    int start, end, ballNum;
+    vector<int> basket;
+
+    cin >> N >> M;
+
+    for (int i = 0; i < N; i++)
+    {
+        basket.push_back(0);
+    }
+
+    for (int i = 0; i < M; i++)
+    {
+        cin >> start >> end >> ballNum;
+
+        for (int startIndex = start - 1; startIndex <= end - 1; startIndex++)
+        {
+            basket[startIndex] = ballNum;
+        }
+    }
+
+    for (int i = 0; i < basket.size(); i++)
+    {
+        cout << basket[i] << ' ';
+    }
+
+    return 0;
+}
